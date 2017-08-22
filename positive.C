b1d5a8c562101f2f551a0a9c309f0871d6033942
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2017-08-22 16:52:09 +0530

    positive.C
    to check the number is positive or negative..

---
+++ positive.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value ");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the given number is positive");
+}
+else
+{printf("the given number is negative");
+}
+getch();
+}
